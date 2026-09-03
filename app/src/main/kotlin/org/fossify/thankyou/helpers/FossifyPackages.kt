5057ad0b4e29932bce0132704b527bb2d4ae3e6e
madebycli <root.cli@proton.me>
2026-09-03 12:45:20 +0200

    Add known Fossify package allowlist

---
+++ app/src/main/kotlin/org/fossify/thankyou/helpers/FossifyPackages.kt
@@ -0,0 +1,27 @@
+package org.fossify.thankyou.helpers
+
+private val knownFossifyPackages = setOf(
+    "org.fossify.calendar",
+    "org.fossify.camera",
+    "org.fossify.clock",
+    "org.fossify.commons.samples",
+    "org.fossify.contacts",
+    "org.fossify.documents",
+    "org.fossify.draw",
+    "org.fossify.filemanager",
+    "org.fossify.flashlight",
+    "org.fossify.gallery",
+    "org.fossify.home",
+    "org.fossify.keyboard",
+    "org.fossify.math",
+    "org.fossify.messages",
+    "org.fossify.musicplayer",
+    "org.fossify.notes",
+    "org.fossify.phone",
+    "org.fossify.thankyou",
+    "org.fossify.voicerecorder",
+)
+
+fun isKnownFossifyPackage(packageName: String): Boolean {
+    return packageName.removeSuffix(".debug") in knownFossifyPackages
+}
